ec1957641f61d06d98312e5f40def3a5ce2e2b1b
dooohun <ehgns0305@naver.com>
2023-08-02 14:56:36 +0900

    환경변수 수정

--- src/services/apiConfig.js
+++ src/services/apiConfig.js
@@ -1,7 +1,7 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
+export const BASE_URL = process.env.REACT_APP_BASE_URL
 
 export const PREPARE_HEADERS = (headers) => {
   headers.set("x-rapidapi-host", "v3.football.api-sports.io");
-  headers.set("x-rapidapi-key", `${process.env.REACT_APP_API_KEY}`);
+  headers.set("x-rapidapi-key", process.env.REACT_APP_API_KEY);
   return headers;
 };
